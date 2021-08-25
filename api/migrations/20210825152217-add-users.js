2994bcb05896f494fc6e55dda9e208beefcaa8ae
Olivier Lidon <olidon@student.42lyon.fr>
2021-08-25 18:16:33 +0200

    added migration for users table

---
+++ api/migrations/20210825152217-add-users.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('users', {
+    id: {
+      type: 'int',
+      primaryKey: true
+    },
+    username: {
+      type: 'string',
+      notNull: true
+    },
+    email: {
+      type: 'string',
+      notNull: true
+    },
+    firstname: {
+      type: 'string',
+      notNull: true
+    },
+    lastname: {
+      type: 'string',
+      notNull: true
+    },
+    gender: 'string',
+    preference: 'string',
+    bio: 'string'
+    // interests: {
+    //   type: 'int',
+    //   foreignKey: {
+    //     name: 'interests_id',
+    //     table: 'interests',
+    //     // rules: {
+    //       // onDelete: 'CASCADE',
+    //       // onUpdate: 'RESTRICT'
+    //     // },
+    //     mapping: 'id'
+    //     }
+    //   }
+    // pictures:{
+    //   type: 'int',
+    //   foreignKey: {
+    //     name: 'pictures_id',
+    //     table: 'puctures',
+    //     // rules: {
+    //       // onDelete: 'CASCADE',
+    //       // onUpdate: 'RESTRICT'
+    //     // },
+    //     mapping: 'id'
+    //     }
+    //   }
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('users');
+};
+
+exports._meta = {
+  "version": 1
+};

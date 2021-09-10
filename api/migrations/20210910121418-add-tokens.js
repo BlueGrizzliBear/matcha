d46adf364be5ecfbbf9566142a269f7a234f2e4f
Tsanuta <olivier.lidon@gmail.com>
2021-09-10 14:20:20 +0200

    Added tokens table to store sessions

---
+++ api/migrations/20210910121418-add-tokens.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.createTable('tokens', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true
+    },
+    token: {
+      type: 'string',
+      notNull: true,
+      unique: true
+    },
+  });
+};
+
+exports.down = function (db) {
+  return db.dropTable('tokens');
+};
+
+exports._meta = {
+  "version": 1
+};

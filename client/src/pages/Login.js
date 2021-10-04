7c915ab96fe1961b4304751286b93701d86505e6
Tsanuta <olivier.lidon@gmail.com>
2021-10-04 17:43:13 +0200

    Add send Link button

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,6 +10,8 @@ function Login(props) {
 	const history = useHistory();
 
 	const [error, setError] = useState(false);
+	const [sent, setSent] = useState(false);
+	const [verification, setVerification] = useState(false);
 
 	const [values, setValues] = useState({
 		username: '',
@@ -20,35 +22,65 @@ function Login(props) {
 
 	const handleLogin = (e) => {
 		e.preventDefault();
-		fetch('http://localhost:9000/login', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			credentials: 'same-origin',
-			body: JSON.stringify({
-				username: values.username,
-				password: values.password
-			}),
-		})
-			.then(res => {
-				if (res.ok && res.status !== 204) {
-					return res.json().then((data) => {
-						localStorage.setItem("token", data.token);
-						props.login();
-						history.push(`/`);
-					})
-				}
-				else if (res.status === 403) {
-					console.log("Account not activated.");
-					setError(true);
-				}
-				else {
-					console.log("Incorrect username or password.");
-					setError(true);
-				}
+		if (!verification) {
+			fetch('http://localhost:9000/login', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				credentials: 'same-origin',
+				body: JSON.stringify({
+					username: values.username,
+					password: values.password
+				}),
 			})
-			.catch(() => {
-				console.log("Fail to login to server");
+				.then(res => {
+					if (res.ok && res.status !== 204) {
+						return res.json().then((data) => {
+							localStorage.setItem("token", data.token);
+							props.login();
+							history.push(`/`);
+						})
+					}
+					else if (res.status === 403) {
+						console.log("Account not activated.");
+						setSent(false);
+						setError(true);
+						setVerification(true);
+					}
+					else {
+						console.log("Incorrect username or password.");
+						setSent(false);
+						setError(true);
+					}
+				})
+				.catch(() => {
+					console.log("Fail to login to server");
+				})
+		}
+		else {
+			fetch('http://localhost:9000/login/activation_link', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				credentials: 'same-origin',
+				body: JSON.stringify({
+					username: values.username,
+				}),
 			})
+				.then(res => {
+					if (res.ok && res.status !== 204) {
+						setSent(true);
+						setVerification(false);
+						setError(false);
+						console.log("Link sent.");
+					}
+					else {
+						console.log("Incorrect username or password.");
+						setError(true);
+					}
+				})
+				.catch(() => {
+					console.log("Fail to send verification link");
+				})
+		}
 	}
 
 	const handleChange = (prop) => (event) => {
@@ -67,11 +99,12 @@ function Login(props) {
 		<>
 			<Box className="FormBox">
 				<Box component="form" noValidate={true} autoComplete="off" onSubmit={handleLogin}>
-					<InputForm error={error} helperText={error && "Incorrect username or password"} label="Username" value={values.username} autoFocus={true} onChange={handleChange('username')} />
+					<InputForm disabled={verification} error={error} helperText={error && !verification && "Incorrect username or password"} label="Username" value={values.username} autoFocus={true} onChange={handleChange('username')} />
 					<PasswordInputForm
-						error={error}
-						helpertext={"Incorrect username or password"}
+						error={sent ? sent : error}
+						helpertext={sent ? "A link has been sent to your email" : (verification ? "Your account email is not verified" : "Incorrect username or password")}
 						filledInputProps={{
+							disabled: verification,
 							label: "Password",
 							value: values.password,
 							type: showPassword ? 'text' : 'password',
@@ -88,7 +121,7 @@ function Login(props) {
 						type="submit"
 						style={{ margin: "16px 0px 4px 0px" }}
 					>
-						Sign in
+						{verification ? 'Send Link' : 'Sign in'}
 					</Button>
 					{/* Ajouter etat pour mettre texte "Email not verified" et bouton pour renvoyer un mail de verification */}
 				</Box>

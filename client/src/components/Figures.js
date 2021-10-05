74bd6ce5adcd8c3c690cabb6822890eb9405fdde
BlueGrizzliBear <clement.bussiere11@gmail.com>
2021-10-05 17:18:52 +0200

    Added the squeletton for location display in front end, added isAddress is back

--- client/src/components/Figures.js
+++ client/src/components/Figures.js
@@ -1,13 +1,9 @@
-import { Box, Chip, Stack, Tooltip } from '@mui/material';
-
+import { useState, useEffect } from 'react';
+import { Box, Chip, Stack, Tooltip, IconButton } from '@mui/material';
 import { makeStyles } from '@mui/styles';
-
 import { Visibility as VisibilityIcon, Favorite as FavoriteIcon, LocationOn as LocationOnIcon } from '@mui/icons-material';
 
 import calculateAge from '../utility/utilities'
-
-import { useState, useEffect } from 'react';
-
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -23,25 +19,6 @@ const useStyles = makeStyles((theme) => ({
 			margin: '0px auto',
 		}
 	},
-	LeftRoot: {
-		display: 'inline',
-		flexWrap: 'wrap',
-		justifyContent: 'space-around',
-		overflow: 'hidden',
-	},
-	RightRoot: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		justifyContent: 'flex-start',
-		alignItems: 'center',
-		overflow: 'hidden',
-	},
-	UpperRoot: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		justifyContent: 'space-around',
-		overflow: 'hidden',
-	},
 	FigureRoot: {
 		display: 'flex',
 		flexWrap: 'wrap',
@@ -56,25 +33,61 @@ function Figures(props) {
 	const classes = useStyles();
 
 	const [user, setUser] = useState(props.user);
-	// const locationMode = 'Manual';
-	// const locationAdress = 'Lyon, France';
 
 	useEffect(() => {
 		setUser(props.user);
 	}, [props.user]);
 
+	const changeAdress = (locationMode) => {
+		if (locationMode === true)
+			return ("Ecully, France");
+		return ("Lyon 7eme, France");
+	}
+
+	const handleLocation = (e) => {
+		console.log(typeof user.location_mode);
+		console.log(user.location_mode);
+		console.log(!user.location_mode);
+
+		fetch('http://localhost:9000/user', {
+			method: 'POST',
+			headers: {
+				'Authorization': "Bearer " + localStorage.getItem("token"),
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				location_mode: !user.location_mode,
+				address: changeAdress(!user.location_mode)
+			})
+		})
+			.then(res => {
+				if (res.ok) {
+					return res.json().then((data) => {
+						setUser(data);
+						props.setValue('user', data)
+					})
+				}
+			})
+			.catch(() => {
+				console.log("Fail to register user to server");
+			})
+
+	}
+
 	return (
 		<Box sx={{ maxWidth: 1552 }} className={classes.root}>
 			<Stack direction="column" spacing={1}>
-				<h2 style={{ 'margin': '4px' }} >{user.firstname} {user.lastname}{user.birth_date ? ", " + calculateAge(user.birth_date) : ''}</h2>
+				<h2 style={{ 'margin': '4px', 'textAlign': 'left', 'paddingLeft': '8px' }} >{user.firstname} {user.lastname}{user.birth_date ? ", " + calculateAge(user.birth_date) : ''}</h2>
 				<Box className={classes.FigureRoot} style={{ 'margin': '4px' }}>
 					<Tooltip title={user.location_mode ? "Automatic" : "Manual"}>
-						<LocationOnIcon />
+						{/* <LocationOnIcon /> */}
+						<IconButton sx={{ 'padding': '2px 8px' }} aria-label="Activate/Desactive Automatic Location" color={user.location_mode ? "primary" : "inherit"} clickable="true" onClick={handleLocation}>
+							<LocationOnIcon />
+						</IconButton>
 					</Tooltip>
 					<Box style={{ 'marginLeft': '8px' }}>{user.address}</Box>
 				</Box>
 			</Stack>
-
 			<Stack direction="row" spacing={2} justifyContent='flex-start' alignItems="center">
 				<Tooltip title="Who liked you">
 					<Chip icon={<FavoriteIcon />} color="primary" label={user.likes} clickable sx={{ fontSize: "20px" }} />

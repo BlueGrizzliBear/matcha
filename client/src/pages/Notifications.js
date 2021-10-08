a011642301e522c437af531fabca7b294e372f37
Tsanuta <olivier.lidon@gmail.com>
2021-10-08 15:33:49 +0200

    Added fetch notifications

--- client/src/pages/Notifications.js
+++ client/src/pages/Notifications.js
@@ -1,8 +1,46 @@
+import { useState, useEffect } from 'react';
+import Loading from '../components/Loading'
+
 function Notifications() {
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+		fetch("http://localhost:9000/notification", {
+			method: 'GET',
+			headers: { 'Authorization': "Bearer " + localStorage.getItem("token") },
+		})
+			.then(res => {
+				if (res.ok && res.status === 200) {
+					return res.json().then((data) => {
+						setIsLoading(false);
+					})
+				}
+				else {
+					console.log("Fail to get notifications");
+					setIsLoading(false);
+				}
+			})
+			.catch(error => {
+				console.log(error);
+				console.log("Fail to fetch");
+				setIsLoading(false);
+			})
+	}, []);
 
 	return (
 		<>
 			<p>THIS IS YOUR NOTIFICATIONS</p>
+
+			{
+				isLoading === true ?
+					Loading()
+					:
+					<>
+
+					</>
+			}
 		</>
 	);
 }

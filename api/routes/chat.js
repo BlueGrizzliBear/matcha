38e03962e89d7ec449af6f2957f9f32099227f9c
Tsanuta <olivier.lidon@gmail.com>
2021-10-29 14:03:57 +0200

    Users must now like each other to send private message

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -46,26 +46,48 @@ router.get('/:id', checkToken, function (req, res, next) {
 
 /* POST /chat/:id/send - Send a message to a user */
 router.post('/:id/send', checkToken, function (req, res, next) {
-	// CHECK IF USERS LIKING MUTUALLY TO GET THE RIGHT TO SEND MESSAGES
-	const chat = new Models.Chat(res.locals.results.id, parseInt(req.params['id']));
 
-	chat.create(req.body.message, (error, results) => {
-		if (error) {
-			console.log(error);
+	const like = new Models.Like(res.locals.results.id, parseInt(req.params['id']));
+	like.liking((likingerr, likingres) => {
+		if (likingerr) {
+			console.log(likingerr);
 			res.status(400).end();
 		}
 		else {
-			const notification = new Models.Notification(parseInt(req.params['id']), 1, results.insertId);
-			notification.create((nerr, nres) => {
-				if (nerr) {
-					console.log(nerr);
+			like.liked((likederr, likedres) => {
+				if (likederr) {
+					console.log(likederr);
 					res.status(400).end();
 				}
 				else {
-					websocket.sendNotification(parseInt(req.params['id']), 1);
-					websocket.sendChat(parseInt(req.params['id']), res.locals.results.id, results.message);
-					res.status(200).end();
+					if (likingres && likedres) {
+						const chat = new Models.Chat(res.locals.results.id, parseInt(req.params['id']));
+
+						chat.create(req.body.message, (error, results) => {
+							if (error) {
+								console.log(error);
+								res.status(400).end();
+							}
+							else {
+								const notification = new Models.Notification(parseInt(req.params['id']), 1, results.insertId);
+								notification.create((nerr, nres) => {
+									if (nerr) {
+										console.log(nerr);
+										res.status(400).end();
+									}
+									else {
+										websocket.sendNotification(parseInt(req.params['id']), 1);
+										websocket.sendChat(parseInt(req.params['id']), res.locals.results.id, results.message);
+										res.status(200).end();
+									}
+								});
+							}
+						});
+					}
+					else
+						res.status(400).end();
 				}
+
 			});
 		}
 	});
